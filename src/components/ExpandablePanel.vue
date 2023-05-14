3923c24d1ff44079d1bcefed51480d03243045f1
陈小伟 <chenxiaoweichinese@gmail.com>
2023-05-14 17:28:27 +0800

    feat: 新增折叠面板组件

---
+++ src/components/ExpandablePanel.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface Props {
+    panelName: string
+}
+
+const props = defineProps<Props>()
+
+const isExpanded = ref(true)
+
+const togglePanel = () => {
+    isExpanded.value = !isExpanded.value
+}
+</script>
+
+<template>
+  <div class="panel">
+    <div class="panel-header" @click="togglePanel">
+      <span>{{ props.panelName }}</span>
+      <div class="triangle" :class="{ rotated: isExpanded }"></div>
+    </div>
+    <div class="panel-content" :class="{expanded: isExpanded}">
+        <div class="slot">
+            <slot></slot>
+        </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.panel {
+    width: 100%;
+background-color: white;
+  border-radius: 4px;
+}
+
+.panel-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: white;
+  cursor: pointer;
+  border-radius: 4px;
+}
+
+.panel-content {
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height 0.3s ease-in-out;
+}
+
+.panel-content.expanded {
+  max-height: 500px;
+}
+
+.slot {
+  padding: 0 20px 10px 20px;
+}
+
+.triangle {
+  width: 0;
+  height: 0;
+  border-left: 6px solid transparent;
+  border-right: 6px solid transparent;
+  border-bottom: 10px solid rgba(56, 79, 194, 1); /* 三角形颜色 */
+  transition: transform 0.3s ease-in-out;
+  cursor: pointer;
+}
+
+.triangle.rotated {
+  transform: rotate(180deg);
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,29 +1,148 @@
 <script setup lang="ts">
 import MessageCard from '@/components/MessageCard.vue';
+import ExpandablePanel from '@/components/ExpandablePanel.vue';
 import { ref } from 'vue'
-const showMessage = ref(false)
-const close = () => {
-  showMessage.value = false
-}
+const loading = ref(false)
+const message = ref('')
+const likedURL = ref('')
+const postsURL = ref('')
+const cookie = ref('')
+
 const open = () => {
-  showMessage.value = !showMessage.value
+  message.value = 'Hello World!'
 }
 </script>
 
 <template>
-  <div class="about">
-    <button @click="open">Show Message</button>
-    <MessageCard message="hhhh" :visible="showMessage" @close="close"/>
+  <LoadAni :loading="loading" />
+  <MessageCard :message="message" :visible="message !== ''" @close="message = ''" />
+  <div class="content">
+    <ExpandablePanel panel-name="高级下载">
+      <div class="advanced-downlaod">
+        <div class="liked-posts">
+          <div class="single-input-container">
+            <input class="single-input" type="text" v-model="likedURL" placeholder="输入框" />
+            <button class="input-button" @click="open">GO</button>
+          </div>
+        </div>
+        <div class="homepage-posts">
+          <div class="connected-input-container">
+            <input class="connected-input first-input" type="text" v-model="postsURL" placeholder="输入框1" />
+            <input class="connected-input second-input" type="text" v-model="cookie" placeholder="输入框2" />
+            <button class="connected-button" @click="open">GO</button>
+          </div>
+        </div>
+      </div>
+    </ExpandablePanel>
+    <div class="control-pannel">
+      <div>内容筛选</div>
+      <div>自动下载</div>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+/* 公共样式 */
+.content {
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
+.advanced-downlaod,
+.control-pannel {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 0.3rem;
+}
+
+.advanced-downlaod .button,
+.control-pannel {
+  margin: 10px 0;
+}
+
+.liked-posts,
+.homepage-posts {
+  width: 100%;
+  margin: 10px 0;
+}
+
+.single-input-container {
+  display: flex;
+}
+
+.single-input {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 5px;
+  outline: none;
+  font-size: 14px;
+  width: 100%;
+}
+
+.input-button {
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px;
+  margin-left: 5px;
+  padding: 6px 12px;
+  cursor: pointer;
+  border-radius: 4px;
+  height: 35px;
+  width: max-content;
+}
+
+.connected-input-container {
+  display: flex;
+}
+
+.connected-input {
+  display: flex;
+  width: 100%;
+  border: 1px solid #ccc;
+  padding: 5px;
+  outline: none;
+  font-size: 14px;
+}
+
+.first-input {
+  width: 70%;
+  border-right: none;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+}
+
+.second-input {
+  width: 30%;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+}
+
+.connected-button {
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px;
+  margin-left: 5px;
+  padding: 6px 12px;
+  cursor: pointer;
+  border-radius: 4px;
+  height: 35px;
+  width: max-content;
+}
+
+@media screen and (max-width: 800px) {
+  .content {
+    width: 80%;
+  }
+}
 </style>
