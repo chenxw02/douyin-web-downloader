587caeeb74c811cdf92415a5c5abf6f22573d532
陈小伟 <chenxiaoweichinese@gmail.com>
2023-05-15 20:29:16 +0800

    Revert "feat: 开关按钮组件和可编辑表格组件"
    This reverts commit 954e8f58c2825dc58e33af0e2e7dc3733879a527.

--- src/components/InputButton.vue
+++ src/components/InputButton.vue
@@ -21,38 +21,40 @@ const submitValue = () => {
 
 <template>
     <div class="single-input-container">
-        <input class="single-input" type="text" :placeholder="props.placeholder" v-model="inputValue" />
+        <input class="single-input" type="text" :placeholder="props.placeholder" v-model="inputValue"/>
         <button class="input-button" @click="submitValue">{{ props.buttonText }}</button>
     </div>
 </template>
 
 <style scoped>
+
 .single-input-container {
-    display: flex;
+  display: flex;
 }
 
 .single-input {
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    padding: 5px;
-    outline: none;
-    font-size: 14px;
-    width: 100%;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 5px;
+  outline: none;
+  font-size: 14px;
+  width: 100%;
 }
 
 .input-button {
-    background-color: rgb(61, 79, 169);
-    border: none;
-    color: white;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 14px;
-    margin-left: 5px;
-    padding: 6px 12px;
-    cursor: pointer;
-    border-radius: 4px;
-    height: 35px;
-    width: max-content;
+  background-color: rgb(61, 79, 169);
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px;
+  margin-left: 5px;
+  padding: 6px 12px;
+  cursor: pointer;
+  border-radius: 4px;
+  height: 35px;
+  width: max-content;
 }
+
 </style>
